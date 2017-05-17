1e324b9d7c53a1c0e133b74426be24582dc2a284
Roberto Paredes <rdparedessalazar@gmail.com>
2017-05-17 19:04:45 -0400

    Don Sling

--- ejercicio.py
+++ ejercicio.py
@@ -4,6 +4,28 @@ class Ejercicio():
   def obtener_divisores(self, numero):
     return [n for n in range(1, numero) if (numero % n == 0)]
 
+  def es_perfecto(self, numero):
+    divisores = self.obtener_divisores(numero)
+    return (numero == sum(divisores))
+
+  def es_abundante(self, numero):
+    divisores = self.obtener_divisores(numero)
+    return (numero < sum(divisores))
+
+  def es_deficiente(self, numero):
+    divisores = self.obtener_divisores(numero)
+    return (numero > sum(divisores))
+
+  def es_primo(self, numero):
+    divisores = [n for n in range(1, numero + 1) if (numero % n == 0)]
+    return (len(divisores) == 2)
+
+  def obtener_perfectos(self, lista_numeros):
+    try:
+      return [n for n in lista_numeros if self.es_perfecto(n)]
+    except TypeError:
+      return []
+
   # def obtener_divisores(self, numero):
     # contador=1
     # divisores=[]
--- test_ejercicio.py
+++ test_ejercicio.py
@@ -3,12 +3,47 @@ from ejercicio import Ejercicio
 
 class TestEjercicio(unittest.TestCase):
 
+  def setUp(self):
+    self.subject = Ejercicio()
+
   def test_deberia_retornar_divisores(self):
-    subject = Ejercicio()
     resultado_esperado_6 = [1, 2, 3]
     resultado_esperado_10 = [1, 2, 5]
-    self.assertEqual(resultado_esperado_6, subject.obtener_divisores(6))
-    self.assertEqual(resultado_esperado_10, subject.obtener_divisores(10))
+    self.assertEqual(resultado_esperado_6, self.subject.obtener_divisores(6))
+    self.assertEqual(resultado_esperado_10, self.subject.obtener_divisores(10))
+
+  def test_deberia_retornar_verdadero_si_el_numero_es_perfecto(self):
+    self.assertTrue(self.subject.es_perfecto(6))
+
+  def test_deberia_retornar_falso_si_el_numero_no_es_perfecto(self):
+    self.assertFalse(self.subject.es_perfecto(7))
+
+  def test_deberia_retornar_solo_numeros_perfectos(self):
+    resultado_esperado = [6]
+    self.assertEqual(resultado_esperado, self.subject.obtener_perfectos([1, 6, 78, 344]))
+
+  def test_deberia_retornar_vacio_si_el_tipo_no_es_correcto(self):
+    self.assertEquals([],self.subject.obtener_perfectos("perfectos"))
+
+  def test_deberia_retornar_verdadero_si_el_numero_es_abundante(self):
+    self.assertTrue(self.subject.es_abundante(12))
+
+  def test_deberia_retornar_falso_si_el_numero_no_es_abundante(self):
+    self.assertFalse(self.subject.es_abundante(6))
+
+  def test_deberia_retornar_verdadero_si_el_numero_es_deficiente(self):
+    self.assertTrue(self.subject.es_deficiente(16))
+
+  def test_deberia_retornar_falso_si_el_numero_no_es_deficiente(self):
+    self.assertFalse(self.subject.es_deficiente(6))
+
+  def test_deberia_retornar_verdadero_si_el_numero_es_primo(self):
+    self.assertTrue(self.subject.es_primo(5))
+    self.assertTrue(self.subject.es_primo(89))
+
+  def test_deberia_retornar_falso_si_el_numero_no_es_primo(self):
+    self.assertFalse(self.subject.es_primo(1))
+    self.assertFalse(self.subject.es_primo(96))
 
 if __name__ == '__main__':
   unittest.main()
